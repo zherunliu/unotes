bec11f6f35f30577bd78dc69782367c60e17d9f0
zherunliu <1437257281@qq.com>
2025-12-25 16:58:46 +0800

    feat: publish page

--- app/src/pages/publish/index.vue
+++ app/src/pages/publish/index.vue
@@ -1,7 +1,111 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { postArticle } from "@/api/article";
+import { reactive, ref } from "vue";
+
+const formData = reactive({
+  title: "",
+  content: "",
+  img: "",
+});
+
+const form = ref();
+
+const rules = {
+  title: {
+    rules: [
+      {
+        required: true,
+        errorMessage: "请输入文章标题",
+      },
+    ],
+  },
+  content: {
+    rules: [
+      {
+        required: true,
+        errorMessage: "请输入文章内容",
+      },
+    ],
+  },
+};
+
+const handleSelect = (e: any) => {
+  const tempFilePath = e.tempFilePaths[0];
+  uni.uploadFile({
+    fileType: "image",
+    url: "http://localhost:3000/upload",
+    filePath: tempFilePath,
+    name: "file",
+    success: (uploadFileRes) => {
+      const data = JSON.parse(uploadFileRes.data);
+      formData.img = `${data.destination}/${data.filename}`;
+    },
+    fail: (err) => {
+      uni.showToast({
+        title: "上传失败",
+        icon: "error",
+        mask: true,
+      });
+      console.error("File upload failed.", err);
+    },
+  });
+};
+
+const submit = () => {
+  form.value
+    .validate()
+    .then(async () => {
+      const res = await postArticle(formData);
+      console.log(res);
+    })
+    .catch((err: any) => {
+      console.log("表单验证失败", err);
+    });
+};
+</script>
 
 <template>
-  <view> </view>
+  <view class="publish">
+    <uni-forms
+      ref="form"
+      :rules="rules"
+      :model="formData"
+      labelWidth="80"
+      label-position="top"
+    >
+      <uni-forms-item required label="文章标题" name="title">
+        <uni-easyinput
+          v-model="formData.title"
+          type="text"
+          placeholder="请输入文章标题"
+        />
+      </uni-forms-item>
+      <uni-forms-item required label="文章内容" name="content">
+        <uni-easyinput
+          v-model="formData.content"
+          type="textarea"
+          placeholder="请输入文章内容"
+        />
+      </uni-forms-item>
+      <uni-forms-item label="上传文件" name="img">
+        <uni-file-picker
+          v-model="formData.img"
+          :limit="1"
+          file-mediatype="image"
+          @select="handleSelect"
+        />
+      </uni-forms-item>
+      <button class="button-primary" @click="submit">提交</button>
+    </uni-forms>
+  </view>
 </template>
 
-<style></style>
+<style lang="scss" scoped>
+.publish {
+  padding: 20rpx;
+}
+.button-primary {
+  background-color: $uni-icon-color-active;
+  color: $uni-text-color-inverse;
+}
+</style>
